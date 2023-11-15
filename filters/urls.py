cb431d6c72efe10c941a8656f07bacdbe3a57528
Azamat <azambylbaev@gmail.com>
2023-11-15 11:45:53 +0300

    last

--- filters/urls.py
+++ filters/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ImageUploadView, ImageToBlackAndWhite, BlurFilter, ImageSketchFilter, EmbossFilter, SharpenFilter, SepiaFilter
+from .views import (ImageUploadView, ImageToBlackAndWhite, BlurFilter,
+                    ImageSketchFilter, EmbossFilter, SharpenFilter, SepiaFilter)
 
 urlpatterns = [
     path('image-upload/', ImageUploadView.as_view(), name='image-upload'),
